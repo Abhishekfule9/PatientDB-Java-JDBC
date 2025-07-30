f396593345a48aadf95cff76914cc2c6e760b10a
Abhishek Fule <abhishekfule9@gmail.com>
2025-07-30 18:59:39 +0530

    Initial commit - Added PatientDB.java

---
+++ PatientDB.java
@@ -0,0 +1,103 @@
+package com.pack1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+public class PatientDB 
+{
+    String driver = "oracle.jdbc.OracleDriver";
+    String dbUrl = "jdbc:oracle:thin:@localhost:1521:xe";
+    String dbUName = "system";
+    String dbPwd = "nareshit";
+
+    Scanner Sc = new Scanner(System.in);
+
+    Connection connect()
+    {
+        Connection con = null;
+        try 
+        {
+            Class.forName(driver);
+            con = DriverManager.getConnection(dbUrl, dbUName, dbPwd);
+            System.out.println("Connection Created !! ");
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    void patient_operation() 
+    {
+        System.out.println("Implementing PreparedStatement\n");
+
+        try 
+        {
+            Connection con = connect();
+            PreparedStatement pstm1 = con.prepareStatement("insert into patient values (?,?,?,?)");
+            PreparedStatement pstm2 = con.prepareStatement("select * from patient");
+            PreparedStatement pstm3 = con.prepareStatement("select * from patient where pid= ? ");
+            PreparedStatement pstm4 = con.prepareStatement("update patient set age = ? where pid =?");
+            PreparedStatement pstm5 = con.prepareStatement("delete from patient where pid=?");
+
+            while (true) 
+            {
+                System.out.println("\nWelcome to Patient Database");
+                System.out.println("1) Add patient data\n2) View patient data\n3) Retrieve patient Data\n4) Update patient data\n5) Delete patient data\n6) Exit");
+                System.out.println("Please enter your Choice :: ");
+                int choice = Integer.parseInt(Sc.nextLine());
+
+                switch (choice) 
+                {
+                    case 1:
+                        System.out.println("Welcome to reception\n : ");
+                        System.out.println("Enter patient id : ");
+                        String pat_id = Sc.nextLine();
+
+                        System.out.println("Enter patient Name : ");
+                        String pat_name = Sc.nextLine();
+
+                        System.out.println("Enter patient Age : ");
+                        int pat_age = Integer.parseInt(Sc.nextLine());
+
+                        System.out.println("Enter patient contact Number : ");
+                        long pat_contact = Long.parseLong(Sc.nextLine());
+
+                        pstm1.setString(1, pat_id);
+                        pstm1.setString(2, pat_name);
+                        pstm1.setInt(3, pat_age);
+                        pstm1.setLong(4, pat_contact);
+
+                        int rowCount = pstm1.executeUpdate();
+                        if (rowCount > 0) 
+                        {
+                            System.out.println("Data is inserted !! ");
+                        } else 
+                        {
+                            System.out.println("Data is not inserted ");
+                        }
+                        break;
+
+                  
+                    case 6:
+                        System.out.println("Exiting...");
+                        return;
+                    default:
+                        System.out.println("Invalid choice!");
+                	}
+            	}
+        	} 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        new PatientDB().patient_operation();
+    }
+}
